9a9e38a6a4e41038bce2df8f58020f25b8733884
FeliciaTorwald <113012104+FeliciaTorwald@users.noreply.github.com>
2024-01-04 19:27:24 +0100

    graphic for player and kettle, uncommented out some code

--- ExpressBrewery/Assets/Scripts/Cauldron.cs
+++ ExpressBrewery/Assets/Scripts/Cauldron.cs
@@ -3,70 +3,70 @@ using UnityEngine;
 
 public class Cauldron : MonoBehaviour
 {
-    public List<HerbTypes> herbsInside = new List<HerbTypes>();
-    bool canInteract;
-    public Player player;
-    private void Update()
-    {
-        if (canInteract)
-        {
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                HerbTypes heldHerb = player.heldHerb;
-                InteractWithCauldron(heldHerb);
-            }
-        }
-    }
+    //public List<HerbTypes> herbsInside = new List<HerbTypes>();
+    //bool canInteract;
+    //public Player player;
+    //private void Update()
+    //{
+    //    if (canInteract)
+    //    {
+    //        if (Input.GetKeyDown(KeyCode.E))
+    //        {
+    //            HerbTypes heldHerb = player.heldHerb;
+    //            InteractWithCauldron(heldHerb);
+    //        }
+    //    }
+    //}
 
-    public void AddHerbToCauldron(HerbTypes herbTypes)
-    {
-        herbsInside.Add(herbTypes);
-        Debug.Log("Added " + herbTypes + " to the cauldron.");
-    }
+    //public void AddHerbToCauldron(HerbTypes herbTypes)
+    //{
+    //    herbsInside.Add(herbTypes);
+    //    Debug.Log("Added " + herbTypes + " to the cauldron.");
+    //}
 
-    public void CheckHerbsInCauldron()
-    {
-        if (herbsInside.Count > 0)
-        {
-            Debug.Log("Herbs in the cauldron:");
-            foreach (var herb in herbsInside)
-            {
-                Debug.Log("- " + herb);
-            }
-        }
-        else
-        {
-            Debug.Log("The cauldron is empty.");
-        }
-    }
+    //public void CheckHerbsInCauldron()
+    //{
+    //    if (herbsInside.Count > 0)
+    //    {
+    //        Debug.Log("Herbs in the cauldron:");
+    //        foreach (var herb in herbsInside)
+    //        {
+    //            Debug.Log("- " + herb);
+    //        }
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("The cauldron is empty.");
+    //    }
+    //}
 
-    public void InteractWithCauldron(HerbTypes heldHerb)
-    {
-        // Check which herb type the player is holding
-        if (heldHerb != HerbTypes.None) // Assume you have a None option in your HerbType enum for no herb
-        {
-            // Add the held herb to the cauldron
-            AddHerbToCauldron(heldHerb);
+    //public void InteractWithCauldron(HerbTypes heldHerb)
+    //{
+    //    // Check which herb type the player is holding
+    //    if (heldHerb != HerbTypes.None) // Assume you have a None option in your HerbType enum for no herb
+    //    {
+    //        // Add the held herb to the cauldron
+    //        AddHerbToCauldron(heldHerb);
 
-            // Check the herbs in the cauldron
-            CheckHerbsInCauldron();
-        }
-        else
-        {
-            Debug.Log("No herb to add to the cauldron.");
-        }
-    }
+    //        // Check the herbs in the cauldron
+    //        CheckHerbsInCauldron();
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("No herb to add to the cauldron.");
+    //    }
+    //}
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            canInteract = true;
-        }
-        else
-        {
-            canInteract = false;
-        }
-    }
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (collision.gameObject.CompareTag("Player"))
+    //    {
+    //        canInteract = true;
+    //    }
+    //    else
+    //    {
+    //        canInteract = false;
+    //    }
+    //}
 }
 
--- ExpressBrewery/Assets/Scripts/Player.cs
+++ ExpressBrewery/Assets/Scripts/Player.cs
@@ -5,8 +5,7 @@ public class Player : MonoBehaviour
     public float speed = 5f; // Adjust this to control movement speed
     private Rigidbody2D rb;
     Vector2 movement;
-    public HerbTypes heldHerb;
-    public Transform hand;
+    public HerbTypes heldHerb = HerbTypes.None;
 
     void Start()
     {
@@ -23,6 +22,22 @@ public class Player : MonoBehaviour
     }
     private void Update()
     {
-        heldHerb = hand.GetComponentInChildren<HerbTypes>();
     }
+        //private HerbTypes GetHerbTypeByName(string herbName)
+        //{
+        //    foreach (Transform child in transform)
+        //    {
+        //        if (child.gameObject.name == herbName)
+        //        {
+        //            // Assuming the herb type is stored in the Herb component (replace Herb with your actual component type)
+        //            //Herb herbComponent = child.GetComponent<Herb>();
+        //        //    if (herbComponent != null)
+        //        //    {
+        //        //        return herbComponent.herbType;
+        //        //    }
+        //        //}
+        //    }
+
+        //    return HerbTypes.None; // Return None if the herb is not found
+        //}
 }
